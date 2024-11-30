a412fa235dc6ddecf7ab946413a12a2186c1b8fb
van-richard <richard.scott.van@gmail.com>
2024-11-30 00:58:33 -0600

    modified:   aliases/slurm.aliases.bash

--- aliases/slurm.aliases.bash
+++ aliases/slurm.aliases.bash
@@ -1,17 +1,29 @@
 #!/bin/bash
 
+q () {
+    squeue --format='%.12i %.10P %.14j %.8u %.12M %.10T %.4D  %R' ${@}
+}
+
 me () {
     # Check SLURM queue for user
-    local username=$USER
-    local iterate=0
-    
-    while getopts 'uir:v' OPTARG; do
+    while getopts 'ui:v' OPTARG; do
         case "${OPTARG}" in 
-            u|user) username=${OPTARG} ;;
-            i|iter) iterate=${OPTARG} ;;
+            u) u=${OPTARG} ;;
+            i) i=${OPTARG} ;;
         esac
     done
-    squeue --format='%.12i %.10P %.14j %.8u %.12M %.10T %.4D  %R' --user=${id}
+
+    if [ ! -z ${u} ]; then
+        username="--user=$USER"
+    else 
+        username="--user=${u}"
+    fi
+
+    if [ -z ${iter} ]; then
+        iterate="--iterate=${iter}"
+    fi
+
+    q $username $iterate ${@}
 }
 
 
