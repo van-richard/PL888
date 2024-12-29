5619dc681e023a847e3508010f4346bd2df2de88
van-richard <richard.scott.van@gmail.com>
2024-12-29 04:41:22 -0600

    cleanup

--- aliases/slurm.aliases.bash
+++ aliases/slurm.aliases.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-function q () {
-    squeue --format='%.12i %.10P %.14j %.8u %.12M %.10T %.4D  %R' ${@}
+alias q="squeue --format='%.12i %.10P %.14j %.8u %.12M %.10T %.4D  %R"
+
+function me () {
+    q --user $USER
 }
 
