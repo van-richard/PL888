61afa4b24c835e8d523603822397a458c5c1affe
van-richard <richard.scott.van@email.com>
2026-06-07 14:00:20 -0500

    fix(aliases): make vcp behave like verbose cp

--- aliases/common/cp.bash
+++ aliases/common/cp.bash
@@ -1,19 +1,50 @@
 #!/bin/bash
 
+_vcp_usage() {
+    printf 'Usage: vcp [cp-options] [--] source [destination ...]\n'
+    printf '       vcp -h | --help\n'
+}
+
 vcp() {
-    if [ $# -eq 0 ]; then
-        echo "Usage: vcp <source> [destination]"
+    local -a opts=()
+    local -a operands=()
+    local arg=""
+
+    while [[ $# -gt 0 ]]; do
+        arg="$1"
+        case "${arg}" in
+            -h|--help)
+                _vcp_usage
+                return 0
+                ;;
+            --)
+                shift
+                break
+                ;;
+            -*)
+                opts+=("${arg}")
+                shift
+                ;;
+            *)
+                break
+                ;;
+        esac
+    done
+
+    while [[ $# -gt 0 ]]; do
+        operands+=("$1")
+        shift
+    done
+
+    if [[ ${#operands[@]} -eq 0 ]]; then
+        _vcp_usage >&2
         return 1
     fi
 
-    local src="$1"
-    local dest="$2"
-
-    if [ -z "$dest" ]; then
-        # no destination given → copy into current directory
-        command cp -v "$src" .
-    else
-        # use cp as normal
-        command cp -v "$src" "$dest"
+    if [[ ${#operands[@]} -eq 1 ]]; then
+        command cp -v "${opts[@]}" -- "${operands[0]}" .
+        return $?
     fi
+
+    command cp -v "${opts[@]}" -- "${operands[@]}"
 }
