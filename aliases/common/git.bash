7b5fd83e1ad7899043524a17efad9e055e37b5aa
van-richard <richard.scott.van@email.com>
2026-06-07 13:49:47 -0500

    feat(aliases): add common git shortcuts

--- aliases/common/git.bash
+++ aliases/common/git.bash
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 alias gs="git status"
+alias ga="git add"
+alias gaa="git add --all"
+alias gcm="git commit -m"
+alias gp="git push"
+alias gpl="git pull"
+alias gb="git branch"
+alias gba="git branch -a"
+alias gd="git diff"
+alias gds="git diff --staged"
+alias gl="git log --oneline --decorate --graph -20"
